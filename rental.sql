c4913d576550c20693982491906672fd4363ad5c
Graham Blandford <grahamblandford@parabola-solutions.com>
2021-02-07 17:01:42 -0500

    Added requests table / more codes entries
    Added rental table
    More codes entries

--- rental.sql
+++ rental.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 06, 2021 at 07:51 PM
+-- Generation Time: Feb 07, 2021 at 01:58 PM
 -- Server version: 8.0.23-0ubuntu0.20.04.1
 -- PHP Version: 7.4.3
 
@@ -21,8 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `rental`
 --
-CREATE DATABASE IF NOT EXISTS `rental` DEFAULT CHARACTER SET utf8;
-USE `rental`;
 
 -- --------------------------------------------------------
 
@@ -39,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `codes` (
   `is_default` int NOT NULL DEFAULT (0),
   PRIMARY KEY (`code_id`),
   UNIQUE KEY `uk_codes_code_type_value` (`code_type`,`code_value`)
-) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=75 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `codes`
@@ -95,194 +93,25 @@ INSERT INTO `codes` (`code_id`, `code_type`, `code_value`, `description`, `is_de
 (53, 'payment_frequency', 'biweekly', 'Bi-weekly', 0),
 (54, 'payment_type', 'etransfer', 'E-transfer', 1),
 (55, 'payment_type', 'cheque', 'Cheque', 0),
-(56, 'payment_type', 'post-dated', 'Post-dated Cheque', 0),
+(56, 'payment_type', 'postdated', 'Post-dated Cheque', 0),
 (57, 'payment_type', 'debitcredit', 'Debit/Credit', 0),
 (58, 'payment_type', 'paypal', 'Paypal', 0),
 (59, 'lease_status', 'active', 'Active', 1),
 (60, 'lease_status', 'expired', 'Expired', 0),
-(61, 'lease_status', 'cancelled', 'Cancelled', 0);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `landlords`
---
-
-DROP TABLE IF EXISTS `landlords`;
-CREATE TABLE IF NOT EXISTS `landlords` (
-  `landlord_id` int NOT NULL AUTO_INCREMENT,
-  `legal_name` varchar(50) NOT NULL,
-  `salutation_code` varchar(10) DEFAULT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `address_1` varchar(50) NOT NULL,
-  `address_2` varchar(50) DEFAULT NULL,
-  `city` varchar(50) NOT NULL,
-  `province_code` varchar(2) NOT NULL DEFAULT (_utf8mb4'ON'),
-  `postal_code` varchar(10) NOT NULL,
-  `phone` varchar(20) NOT NULL,
-  `fax` varchar(20) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `sms` varchar(20) NOT NULL,
-  `status_code` varchar(10) NOT NULL,
-  `last_updated` datetime NOT NULL DEFAULT (now()),
-  `last_updated_user_id` varchar(50) NOT NULL,
-  PRIMARY KEY (`landlord_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `landlord_rental_properties`
---
-
-DROP TABLE IF EXISTS `landlord_rental_properties`;
-CREATE TABLE IF NOT EXISTS `landlord_rental_properties` (
-  `landlord_rental_property_id` int NOT NULL AUTO_INCREMENT,
-  `landlord_id` int NOT NULL,
-  `rental_property_id` int NOT NULL,
-  PRIMARY KEY (`landlord_rental_property_id`),
-  KEY `fk_landlord_rental_properties_rental_properties` (`rental_property_id`),
-  KEY `fk_landlord_rental_properties_landlords` (`landlord_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `leases`
---
-
-DROP TABLE IF EXISTS `leases`;
-CREATE TABLE IF NOT EXISTS `leases` (
-  `lease_id` int NOT NULL AUTO_INCREMENT,
-  `rental_property_id` int NOT NULL,
-  `tenant_id` int NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date DEFAULT NULL,
-  `payment_day` int NOT NULL DEFAULT (1),
-  `payment_frequency_code` varchar(20) NOT NULL,
-  `base_rent_amount` decimal(10,2) NOT NULL DEFAULT (0),
-  `parking_amount` decimal(10,2) NOT NULL DEFAULT (0),
-  `other_amount` decimal(10,2) NOT NULL DEFAULT (0),
-  `payable_to` varchar(50) NOT NULL,
-  `deposit_amount` decimal(10,2) NOT NULL DEFAULT (0),
-  `key_deposit` decimal(10,2) NOT NULL DEFAULT (0),
-  `payment_type_code` varchar(20) NOT NULL,
-  `include_electricity` tinyint(1) NOT NULL DEFAULT (0),
-  `include_heat` tinyint(1) NOT NULL DEFAULT (0),
-  `include_water` tinyint(1) NOT NULL DEFAULT (0),
-  `insurancy_policy_number` varchar(50) DEFAULT NULL,
-  `status_code` varchar(20) NOT NULL,
-  `last_updated` datetime NOT NULL DEFAULT (now()),
-  `last_updated_user_id` varchar(50) NOT NULL,
-  PRIMARY KEY (`lease_id`),
-  KEY `fk_leases_rental_properties` (`rental_property_id`),
-  KEY `fk_leases_tenants` (`tenant_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `rental_properties`
---
-
-DROP TABLE IF EXISTS `rental_properties`;
-CREATE TABLE IF NOT EXISTS `rental_properties` (
-  `rental_property_id` int NOT NULL AUTO_INCREMENT,
-  `address_1` varchar(50) NOT NULL,
-  `address_2` varchar(50) DEFAULT NULL,
-  `city` varchar(50) NOT NULL,
-  `province_code` varchar(2) NOT NULL DEFAULT (_utf8mb4'ON'),
-  `postal_code` varchar(10) NOT NULL,
-  `latitude` varchar(20) NOT NULL,
-  `longitude` varchar(20) NOT NULL,
-  `number_bedrooms` int NOT NULL,
-  `property_type_code` varchar(20) NOT NULL,
-  `parking_space_type_code` varchar(20) NOT NULL,
-  `number_parking_space` int NOT NULL DEFAULT (0),
-  `rental_duration_code` varchar(20) NOT NULL DEFAULT (_utf8mb4'monthly'),
-  `smoking_allowed` tinyint(1) NOT NULL DEFAULT (0),
-  `insurance_required` tinyint(1) NOT NULL DEFAULT (1),
-  `status_code` varchar(20) NOT NULL,
-  `last_updated` datetime NOT NULL DEFAULT (now()),
-  `last_updated_user_id` varchar(50) NOT NULL,
-  PRIMARY KEY (`rental_property_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tenants`
---
-
-DROP TABLE IF EXISTS `tenants`;
-CREATE TABLE IF NOT EXISTS `tenants` (
-  `tenant_id` int NOT NULL AUTO_INCREMENT,
-  `salutation_code` varchar(10) DEFAULT NULL,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  `address_1` varchar(50) NOT NULL,
-  `address_2` varchar(50) DEFAULT NULL,
-  `city` varchar(50) NOT NULL,
-  `province_code` varchar(2) NOT NULL DEFAULT (_utf8mb4'ON'),
-  `postal_code` varchar(10) NOT NULL,
-  `phone` varchar(20) NOT NULL,
-  `fax` varchar(20) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `date_of_birth` date NOT NULL,
-  `gender` varchar(10) NOT NULL,
-  `social_insurance_number` varchar(10) DEFAULT NULL,
-  `status_code` varchar(10) NOT NULL,
-  `last_updated` datetime NOT NULL DEFAULT (now()),
-  `last_updated_user_id` varchar(50) NOT NULL,
-  PRIMARY KEY (`tenant_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` varchar(255) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `user_type_code` varchar(10) NOT NULL,
-  `status_code` varchar(10) NOT NULL,
-  `last_login` datetime NOT NULL DEFAULT (now()),
-  `last_updated` datetime NOT NULL DEFAULT (now()),
-  `last_updated_user_id` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`user_id`, `password`, `email`, `user_type_code`, `status_code`, `last_login`, `last_updated`, `last_updated_user_id`) VALUES
-('admin', 'admin', 'admin@localhost', 'admin', 'enabled', '2021-02-06 18:06:45', '2021-02-06 18:06:45', NULL),
-('landlord', 'tenant', 'landlord@localhost', 'admin', 'enabled', '2021-02-06 18:06:45', '2021-02-06 18:06:45', NULL),
-('tenant', 'tenant', 'tenant@localhost', 'admin', 'enabled', '2021-02-06 18:06:45', '2021-02-06 18:06:45', NULL);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `landlord_rental_properties`
---
-ALTER TABLE `landlord_rental_properties`
-  ADD CONSTRAINT `fk_landlord_rental_properties_landlords` FOREIGN KEY (`landlord_id`) REFERENCES `landlords` (`landlord_id`),
-  ADD CONSTRAINT `fk_landlord_rental_properties_rental_properties` FOREIGN KEY (`rental_property_id`) REFERENCES `rental_properties` (`rental_property_id`);
-
---
--- Constraints for table `leases`
---
-ALTER TABLE `leases`
-  ADD CONSTRAINT `fk_leases_rental_properties` FOREIGN KEY (`rental_property_id`) REFERENCES `rental_properties` (`rental_property_id`),
-  ADD CONSTRAINT `fk_leases_tenants` FOREIGN KEY (`tenant_id`) REFERENCES `tenants` (`tenant_id`);
+(61, 'lease_status', 'cancelled', 'Cancelled', 0),
+(62, 'request_type', 'repair', 'Repair', 1),
+(63, 'request_type', 'replacement', 'Replacement', 0),
+(64, 'request_type', 'complaint', 'Complaint', 0),
+(65, 'request_type', 'other', 'Other', 0),
+(66, 'request_status', 'new', 'New', 1),
+(67, 'request_status', 'received', 'Received', 0),
+(68, 'request_status', 'inprogress', 'In progress', 0),
+(69, 'request_status', 'completed', 'Completed', 0),
+(70, 'request_priority', 'low', 'Low', 1),
+(71, 'request_priority', 'medium', 'Medium', 0),
+(72, 'request_priority', 'high', 'High', 0),
+(73, 'request_solution', 'repaired', 'Repaired', 1),
+(74, 'request_solution', 'replaced', 'Replaced', 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
