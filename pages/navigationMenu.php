af355985fde9dedebee6a9f2b8f247d2814d2b3c
TaehyungAlexKim <63694948+TaehyungAlexKim@users.noreply.github.com>
2021-02-11 11:10:27 -0500

    Update navigationMenu.php

--- pages/navigationMenu.php
+++ pages/navigationMenu.php
@@ -11,30 +11,31 @@ $base_URL = ($_SERVER['HTTPS'] == 'on') ? 'https://' : 'http://';
 $base_URL .= $_SERVER['HTTP_HOST'];
 ?>
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="<?php echo $base_URL."/index.php"?>">RentalBuddy</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<?php echo $base_URL."/index.php"?>">RentalBuddy</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-      <ul class="navbar-nav me-auto mb-2 mb-md-0">
-        <li class="nav-item active">
-          <a class="nav-link" aria-current="page" href="<?php echo $base_URL."/index.php"?>">Home</a>
-        </li>
-        <li class="nav-item">
-
-          <a class="nav-link" href="<?php echo $base_URL.""?>">Tenants</a>
-
-          <a class="nav-link" href="<?php echo $base_URL."/pages/service_request.php"?>">Service Request</a>>>>>>>> Stashed changes
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo $base_URL."/pages/landlords.php"?>">Landlords</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?php echo $base_URL.""?>">Properties</a>
-        </li>
-      </ul>
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item active">
+                    <a class="nav-link" aria-current="page" href="<?php echo $base_URL."/index.php"?>">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $base_URL.""?>">Tenants</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $base_URL."/pages/service_request.php"?>">Service Request(TEMP)</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $base_URL."/pages/landlords.php"?>">Landlords</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $base_URL.""?>">Properties</a>
+                </li>
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
