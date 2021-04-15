2ea1517651f5362cff8aaf70f7d76cc204c8bda5
hojumoney10 <hojumoney10@gmail.com>
2021-04-15 11:16:46 -0400

    lat/lng added again

--- pages/rental_properties.php
+++ pages/rental_properties.php
@@ -587,10 +587,10 @@ function formRentalProperty()
                 <!-- lat & long -->
                 <div class="input-group">
                     <label for="latitude">Lat./Long.</label>
-                    <input type="text" size="15" maxlength="15" style="max-width: 50%;" class="form-control" id="latitude" name="latitude" aria-describedby="latitude-help" placeholder="Enter latitude" value="<?php echo $row['latitude']; ?>"<?php echo ($_SESSION['PAGEMODE'] == 'VIEW') ? " readonly" : "" ?>>
+                    <input type="text" size="15" maxlength="15" style="max-width: 50%;" class="form-control" id="latitude" name="latitude" aria-describedby="latitude-help" placeholder="" value="<?php echo $row['latitude']; ?>" readonly>
                     <small id="latitude-help" class="form-text text-muted"></small>
-
-                    <input type="text" size="15" maxlength="15" style="max-width: 50%;" class="form-control" id="longitude" name="longitude" aria-describedby="longitude-help" placeholder="Enter longitude" value="<?php echo $row['longitude']; ?>"<?php echo ($_SESSION['PAGEMODE'] == 'VIEW') ? " readonly" : "" ?>>
+                    
+                    <input type="text" size="15" maxlength="15" style="max-width: 50%;" class="form-control" id="longitude" name="longitude" aria-describedby="longitude-help" placeholder="" value="<?php echo $row['longitude']; ?>" readonly>
                     <small id="longitude-help" class="form-text text-muted"></small>
                 </div>
 
